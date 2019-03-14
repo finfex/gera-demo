dfb64504d012821a251909d32789bc1c5bc04437
Danil Pismenny <danil@brandymint.ru>
2019-03-14 20:18:50 +0300

    sidekiq password

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,8 +1,12 @@
 Bugsnag.configure do |config|
-  config.api_key = "21e3314644215ae15060a7523b396e83"
+  config.api_key = Rails.application.credentials[:bugsnag_api_key]
   config.app_version = AppVersion.format('%M.%m.%p') # rubocop:disable Style/FormatStringToken
   config.notify_release_stages = %w[production staging]
   config.ignore_classes << ActiveRecord::RecordInvalid
   config.send_code = true
   config.send_environment = true
+end
+
+Bugsnag.before_notify_callbacks << lambda do |report|
+  report.add_tab :context, request_id: Thread.current[:request_id] if Thread.current[:request_id].present?
 end
