7008ee69561508370247a4a70a45cfaf12335d5b
Danil Pismenny <danil@brandymint.ru>
2019-03-16 14:54:36 +0300

    translations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ require 'factory_bot'
 
 puts 'Seed database'
 
-FactoryBot.create :rate_source_exmo
-FactoryBot.create :rate_source_cbr
-FactoryBot.create :rate_source_cbr_avg
-FactoryBot.create :rate_source_manual
-FactoryBot.create :rate_source_bitfinex
+FactoryBot.create :rate_source_exmo, name: 'exmo'
+FactoryBot.create :rate_source_cbr, name: 'Russian Central Bank'
+FactoryBot.create :rate_source_cbr_avg, name: 'Russian Central Bank (avg)'
+FactoryBot.create :rate_source_manual, name: 'Manual'
+FactoryBot.create :rate_source_bitfinex, name: 'bitfinex'
 
 Money::Currency.all.each { |cur| Gera::PaymentSystem.create! name: cur.name, currency: cur }
 
