733463d5cb43cffabda37757b71eecc1442d8769
Danil Pismenny <danil@brandymint.ru>
2021-04-29 09:41:16 +0300

    Adopt to lase version of gera

--- config/initializers/activerecord.rb
+++ config/initializers/activerecord.rb
@@ -1,3 +1,3 @@
-ENV['SCHEMA'] = Rails.root.join('db', ['schema', ActiveRecord::Base.configurations[Rails.env]['adapter'], 'rb'].join('.')).to_s
+ENV['SCHEMA'] = Rails.root.join('db', ['schema', ActiveRecord::Base.configurations[Rails.env].symbolize_keys[:adapter], 'rb'].join('.')).to_s
 
 puts "Use database adapter: #{ActiveRecord::Base.configurations[Rails.env]['adapter']}"
